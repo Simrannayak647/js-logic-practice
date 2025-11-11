84e1cfed5991e015987886fbf5b3ba83256dd419
Simran Nayak <simrannayak763@gmail.com>
2025-11-11 11:47:07 +0000

    First Commit

---
+++ ConditionalThinking/number.js
@@ -0,0 +1,9 @@
+let x = 1000;
+
+if(x >  0){
+   console.log("The number is Positive",x);
+} else if(x < 0){
+   console.log("The number is negative number");
+}else{
+   console.log("The number is zero",x);
+}
